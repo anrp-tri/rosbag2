663679a253c393c751e15b26629ed6a6cb703bb0
Geoffrey Biggs <gbiggs@killbots.net>
2021-10-15 09:33:32 +0900

    Simple hacky prototype
    Signed-off-by: Geoffrey Biggs <gbiggs@killbots.net>

--- rosbag2_transport/include/rosbag2_transport/recorder.hpp
+++ rosbag2_transport/include/rosbag2_transport/recorder.hpp
@@ -23,6 +23,8 @@
 #include <utility>
 #include <vector>
 
+#include "example_interfaces/msg/empty.hpp"
+
 #include "rclcpp/node.hpp"
 #include "rclcpp/qos.hpp"
 
@@ -124,6 +126,7 @@ private:
   std::unordered_map<std::string, rclcpp::QoS> topic_qos_profile_overrides_;
   std::unordered_set<std::string> topic_unknown_types_;
   rclcpp::Service<rosbag2_interfaces::srv::Snapshot>::SharedPtr srv_snapshot_;
+  rclcpp::Publisher<example_interfaces::msg::Empty>::SharedPtr event_pub_;
 };
 
 }  // namespace rosbag2_transport
