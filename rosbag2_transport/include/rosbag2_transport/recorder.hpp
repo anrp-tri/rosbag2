1ac9a4fc9504f314d8178aa61ad4e58641d84ef3
Geoffrey Biggs <gbiggs@killbots.net>
2021-10-20 15:05:06 +0900

    Improved prototype with more flexibility and event information
    Signed-off-by: Geoffrey Biggs <gbiggs@killbots.net>

--- rosbag2_transport/include/rosbag2_transport/recorder.hpp
+++ rosbag2_transport/include/rosbag2_transport/recorder.hpp
@@ -23,14 +23,14 @@
 #include <utility>
 #include <vector>
 
-#include "example_interfaces/msg/empty.hpp"
-
 #include "rclcpp/node.hpp"
 #include "rclcpp/qos.hpp"
 
 #include "rosbag2_cpp/writer.hpp"
 
 #include "rosbag2_interfaces/srv/snapshot.hpp"
+
+#include "rosbag2_interfaces/msg/output_file_split_event.hpp"
 
 #include "rosbag2_storage/topic_metadata.hpp"
 
@@ -126,7 +126,7 @@ private:
   std::unordered_map<std::string, rclcpp::QoS> topic_qos_profile_overrides_;
   std::unordered_set<std::string> topic_unknown_types_;
   rclcpp::Service<rosbag2_interfaces::srv::Snapshot>::SharedPtr srv_snapshot_;
-  rclcpp::Publisher<example_interfaces::msg::Empty>::SharedPtr event_pub_;
+  rclcpp::Publisher<rosbag2_interfaces::msg::OutputFileSplitEvent>::SharedPtr split_event_pub_;
 };
 
 }  // namespace rosbag2_transport
